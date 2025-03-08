8a326915f1d5eced63655b439111c52fff9c2b97
Ankit <ankitchaudhari8600@gmail.com>
2025-03-08 12:30:03 +0530

    Java Practice Code

---
+++ src/Basic_Java/LenghtOfString.java
@@ -0,0 +1,16 @@
+package Basic_Java;
+
+public class LenghtOfString {
+
+    public static void main(String[] args){
+
+        String name = "Ankit";
+        int count = 0;
+        for (int i=0;i<name.length();i++){
+             count++;
+        }
+        System.out.println(count);
+    }
+
+
+}
